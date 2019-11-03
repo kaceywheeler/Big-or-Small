4173ef438551d7d84f02f8e5a3ddcb6a958c6683
Kacey Wheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-03 13:49:57 -0700

    Update bigorsmall.py

--- bigorsmall.py
+++ bigorsmall.py
@@ -4,14 +4,20 @@
 #Unit 8 Labs
 #Big or Small
 
+#Gets user input for a 
 a = int(input("Enter an integer for a: "))
+
+#Gets user input for b
 b = int(input("Enter an integer for b: "))
 
+#If a is greater than b, output yes
 if a > b:
     print("Yes")
 
+#If a is less than b, output no
 elif a < b:
     print("No")
 
+#If a and b are the same, output aplus
 elif a == b:
     print("aplus")
