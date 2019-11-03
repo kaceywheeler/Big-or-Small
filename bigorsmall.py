8fc9930a653ea4c0a2a584dbb805a3c49b1c4cf3
Kacey Wheeler <57329180+kaceywheeler@users.noreply.github.com>
2019-11-03 13:48:23 -0700

    Add files via upload

---
+++ bigorsmall.py
@@ -0,0 +1,17 @@
+#Kacey Wheeler
+#AP Computer Science: 2A
+#2/19/19
+#Unit 8 Labs
+#Big or Small
+
+a = int(input("Enter an integer for a: "))
+b = int(input("Enter an integer for b: "))
+
+if a > b:
+    print("Yes")
+
+elif a < b:
+    print("No")
+
+elif a == b:
+    print("aplus")
